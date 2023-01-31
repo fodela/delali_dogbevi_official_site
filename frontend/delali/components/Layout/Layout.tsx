851de0f56bd728180d2d22f319747e18ede77839
fodela <particleme30@gmail.com>
2023-01-31 11:54:12 +0000

    refactor: layout

--- frontend/delali/components/Layout/Layout.tsx
+++ frontend/delali/components/Layout/Layout.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "../Navigation/Navigation";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-	return (
-		<div className="flex">
-			<Navigation />
-			<div className="flex flex-col justify-between ">
-				<main>{children}</main>
-				<footer>Footer</footer>
-			</div>
-		</div>
-	);
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  return (
+    <div className=" w-screen">
+      {isNavOpen && <Navigation />}
+
+      <main className="p-4 w-full">{children}</main>
+      <footer className="fixed bottom-0">Footer</footer>
+    </div>
+  );
 };
 export default Layout;
