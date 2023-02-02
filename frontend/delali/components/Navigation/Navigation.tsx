a6e67540376b01ffe8ca5f988f5cc90a4ed76409
fodela <particleme30@gmail.com>
2023-02-02 11:57:10 +0000

    refactor: navigation

--- frontend/delali/components/Navigation/Navigation.tsx
+++ frontend/delali/components/Navigation/Navigation.tsx
@@ -3,25 +3,25 @@ import Image from "next/image";
 import NavigationLink from "./NavigationLink";
 import NavBottom from "./NavBottom/NavBottom";
 const Navigation = () => {
-	return (
-		<nav className="bg-[rgb(24,24,24)] flex flex-col h-screen w-[130px] divide-y divide-gray-700 ">
-			<div className="flex flex-col">
-				{/* <Image src="logo.png" /> */}
-				<img src="logo.png" alt="" />
+  return (
+    <nav className="flex flex-col h-screen w-[130px] divide-y divide-custom_white ">
+      <div className="flex flex-col">
+        {/* <Image src="logo.png" /> */}
+        <img src="logo.png" alt="" />
 
-				<p className="text-[rgb(165,165,165)] text-sm text-center py-1">
-					Delali Dogbevi
-				</p>
-			</div>
-			<NavigationLink name="About" link="/about" />
-			<NavigationLink name="Skills" link="/skills" />
-			<NavigationLink name="Work" link="/works" />
-			<NavigationLink name="Blog" link="/blog" />
-			<NavigationLink name="Contact" link="/contact" />
+        <p className="text-[rgb(165,165,165)] text-sm text-center py-1">
+          Delali Dogbevi
+        </p>
+      </div>
+      <NavigationLink name="About" link="/about" />
+      <NavigationLink name="Skills" link="/skills" />
+      <NavigationLink name="Work" link="/works" />
+      <NavigationLink name="Blog" link="/blog" />
+      <NavigationLink name="Contact" link="/contact" />
 
-			<NavBottom />
-		</nav>
-	);
+      <NavBottom />
+    </nav>
+  );
 };
 
 export default Navigation;
