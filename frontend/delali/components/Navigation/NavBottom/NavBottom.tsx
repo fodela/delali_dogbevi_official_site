007407f1069211504261d5e303bf0f31326138f1
fodela <particleme30@gmail.com>
2023-02-03 13:13:48 +0000

    fix: Nav Icon type error

--- frontend/delali/components/Navigation/NavBottom/NavBottom.tsx
+++ frontend/delali/components/Navigation/NavBottom/NavBottom.tsx
@@ -1,21 +1,32 @@
-import NavIcon from "./NavIcon";
 import { BsGithub, BsTwitter } from "react-icons/bs";
 import { FaLinkedinIn } from "react-icons/fa";
 
 const NavBottom = () => {
-	return (
-		<div className="flex justify-between p-4">
-			<NavIcon
-				icon={<FaLinkedinIn size="20" />}
-				link="https://www.linkedin.com/in/delali-dogbevi/"
-			/>
-			<NavIcon icon={<BsGithub size="20" />} link="https://github.com/fodela" />
-			<NavIcon
-				icon={<BsTwitter size="20" />}
-				link="https://twitter.com/Fodela2"
-			/>
-		</div>
-	);
+  return (
+    <div className="flex justify-between p-4">
+      <a
+        className="text-[rgb(165,165,165)] justify-center"
+        href="https://www.linkedin.com/in/delali-dogbevi/"
+        target="_blank"
+      >
+        <FaLinkedinIn size="20" />
+      </a>
+      <a
+        className="text-[rgb(165,165,165)] justify-center"
+        href="https://github.com/fodela"
+        target="_blank"
+      >
+        <BsGithub size="20" />
+      </a>
+      <a
+        className="text-[rgb(165,165,165)] justify-center"
+        href="https://twitter.com/Fodela2"
+        target="_blank"
+      >
+        <BsTwitter size="20" />
+      </a>
+    </div>
+  );
 };
 
 export default NavBottom;
