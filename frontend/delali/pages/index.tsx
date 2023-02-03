de14d6d164755b3f7e3af7002927991154fb7a3a
fodela <particleme30@gmail.com>
2023-02-03 11:59:04 +0000

    refactor: home page

--- frontend/delali/pages/index.tsx
+++ frontend/delali/pages/index.tsx
@@ -1,16 +1,31 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Hero from "../components/Hero";
 import Layout from "../components/Layout/Layout";
+import Portfolio from "../components/Portfolio/Portfolio";
+import Skills from "../components/Skills/Skills";
 const Home: NextPage = () => {
-	return (
-		<>
-			<Head>
-				<title>Delali Dogbevi | Home</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<Layout>From Layout</Layout>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Delali Dogbevi | Home</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Layout>
+        <Hero />
+        <Portfolio />
+        <About />
+        <Skills />
+        <Contact />
+      </Layout>
+    </>
+  );
 };
 
 export default Home;
