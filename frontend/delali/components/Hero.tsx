cda516c44aca76ac6171fa5fb2d451b87c649d96
fodela <particleme30@gmail.com>
2023-02-24 20:58:23 +0000

    refactor: about hero

--- frontend/delali/components/Hero.tsx
+++ frontend/delali/components/Hero.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { BsArrowBarDown, BsArrowDown } from "react-icons/bs";
 import GlassyButton from "./GlassyButton";
+import { Link } from "react-scroll";
 
 const Hero = () => {
   return (
-    <section className=" hero">
+    <section className=" hero " id="home">
       <div className="header_text">
-        <h1 className="text-5xl font-bold text-white">
+        <h1 className="heading_primary">
           Hi,
           <br />
           I'm Delali,
@@ -13,13 +15,23 @@ const Hero = () => {
           web developer
         </h1>
         <h5>FullStack / Django / Flask / React</h5>
-        <GlassyButton name="Contact Me!" glow="#f00" />
+        {/* <Link
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+        > */}
+        <GlassyButton name="Contact Me!" glow="#00ff" link="#contact" />
+        {/* </Link> */}
       </div>
-      <div className=" absolute bottom-0 left-0">
-        <span className="animate-bounce ">scroll down</span> <span>&rarr;</span>
+
+      <div className="absolute bottom-8 -left-7 scroll_down  animate-bounce ">
+        <p>scroll down </p> <BsArrowDown />
       </div>
-      <div className=" bottom-0 right-0 absolute  flex rotate-90">
-        <span className="animate-bounce ">scroll down</span> <span>&rarr;</span>
+      <div className=" absolute bottom-8 -right-7 scroll_down animate-bounce ">
+        <p>scroll down </p> <BsArrowDown />
       </div>
     </section>
   );
