662b9b9e9ba68332abd6c0e2bda7edc87d820de0
fodela <particleme30@gmail.com>
2023-02-27 21:01:57 +0000

    refactor portfolio

--- frontend/delali/components/Portfolio/Portfolio.tsx
+++ frontend/delali/components/Portfolio/Portfolio.tsx
@@ -4,43 +4,44 @@ import ProjectCard from "./ProjectCard";
 
 const projects = [
   {
-    title: "Effect",
+    title: "Omnifood",
     description:
-      "A productivity app that combines the Einshover and Pomodoro Technique to help user prioritize their work and improve their focus",
+      "The smart 365-days-per-year food subscription that will make you eat healthy again. Tailored to your personal tastes and nutritional needs. We have delivered 250,000+ meals last year!",
     thumbnailLink: "#",
-    liveLink: "",
+    liveLink: "https://omnifood-fo.netlify.app/",
     stack: ["Python", "Flask", "React"],
-    gifLink: "",
+    gifLink: "omnifood.gif",
   },
   {
     title: "Effect",
     description:
       "A productivity app that combines the Einshover and Pomodoro Technique to help user prioritize their work and improve their focus",
-    thumbnailLink: "#",
-    liveLink: "",
+    thumbnailLink: "effect.gif",
+    liveLink: "https://effect-fo.vercel.app/",
     stack: ["Python", "Flask", "React"],
-    gifLink: "",
+    gifLink: "effect.gif",
   },
   {
-    title: "Effect",
+    title: "Thykel-Assistant",
     description:
-      "A productivity app that combines the Einshover and Pomodoro Technique to help user prioritize their work and improve their focus",
-    thumbnailLink: "#",
-    liveLink: "",
-    stack: ["Python", "Flask", "React"],
-    gifLink: "",
+      "Cutting-edge voice recognition technology. Just speak naturally and let ChatGPT do the rest, whether you're a student, researcher, or simply curious. Experience the power of voice-driven AI technology for yourself!",
+    thumbnailLink: "effect.gif",
+    liveLink: "coming soon.",
+    stack: ["React", "Typescript", "Firebase"],
+    gifLink: "effect.gif",
   },
 ];
 
 const Portfolio = () => {
   return (
     <section>
-      <h2 className="heading_secondary">My Portfolio</h2>
+      <div className="bg_header">Works</div>
+      <h2 className="heading_secondary">What I have done</h2>
       <p>A small gallery of my recent projects</p>
       <GlassyButton name="See More!" />
       <div className="flex flex-wrap items-center justify-center gap-6">
-        {projects.map((project) => (
-          <ProjectCard project={project} />
+        {projects.map((project, index) => (
+          <ProjectCard project={project} key={index} />
         ))}
       </div>
     </section>
