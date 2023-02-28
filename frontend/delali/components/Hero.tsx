fd3570c3aa4fb3d412ba3724a84dd65276aa5fbe
fodela <particleme30@gmail.com>
2023-02-28 07:23:31 +0000

    🛠:refactor: hero

--- frontend/delali/components/Hero.tsx
+++ frontend/delali/components/Hero.tsx
@@ -2,18 +2,35 @@ import React from "react";
 import { BsArrowBarDown, BsArrowDown } from "react-icons/bs";
 import GlassyButton from "./GlassyButton";
 import { Link } from "react-scroll";
+import Image from "next/image";
+// import { useSession } from "next-auth/react";
 
 const Hero = () => {
+  // const { data: session } = useSession();
   return (
-    <section className=" hero " id="home">
-      <div className="header_text">
-        <h1 className="heading_primary">
-          Hi,
-          <br />
-          I'm Delali,
-          <br />
-          web developer
-        </h1>
+    <section className="" id="home">
+      <div className="">
+        <div className="relative">
+          {/* <div className="absolute right-0 top">
+            <Image
+              className=""
+              src="/images/hero.png"
+              width={250}
+              height={250}
+              alt="a headshot of delali smiling"
+            />
+          </div> */}
+          <h1
+            className="
+          heading_primary"
+          >
+            Hi,
+            <br />
+            I'm Delali,
+            <br />
+            web developer
+          </h1>{" "}
+        </div>
         <h5>FullStack / Django / Flask / React</h5>
         {/* <Link
           activeClass="active"
@@ -23,7 +40,12 @@ const Hero = () => {
           offset={50}
           duration={500}
         > */}
-        <GlassyButton name="Contact Me!" glow="#00ff" link="#contact" />
+        <GlassyButton name="Contact Me!" glow="#3cc39f" link="#contact" />
+        {/* {session ? (
+          <GlassyButton name="Sign out" />
+        ) : (
+          <GlassyButton name="Sign in" />
+        )} */}
         {/* </Link> */}
       </div>
 
