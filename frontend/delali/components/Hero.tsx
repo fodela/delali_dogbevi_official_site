0e2b3ceca2300df23d7b72d94b901d3efe6374a3
fodela <particleme30@gmail.com>
2023-03-01 07:45:31 +0000

    🔧 chore: clean up hero component

--- frontend/delali/components/Hero.tsx
+++ frontend/delali/components/Hero.tsx
@@ -11,15 +11,6 @@ const Hero = () => {
     <section className="" id="home">
       <div className="">
         <div className="relative">
-          {/* <div className="absolute right-0 top">
-            <Image
-              className=""
-              src="/images/hero.png"
-              width={250}
-              height={250}
-              alt="a headshot of delali smiling"
-            />
-          </div> */}
           <h1
             className="
           heading_primary"
@@ -32,21 +23,8 @@ const Hero = () => {
           </h1>{" "}
         </div>
         <h5>FullStack / Django / Flask / React</h5>
-        {/* <Link
-          activeClass="active"
-          to="contact"
-          spy={true}
-          smooth={true}
-          offset={50}
-          duration={500}
-        > */}
+
         <GlassyButton name="Contact Me!" glow="#3cc39f" link="#contact" />
-        {/* {session ? (
-          <GlassyButton name="Sign out" />
-        ) : (
-          <GlassyButton name="Sign in" />
-        )} */}
-        {/* </Link> */}
       </div>
 
       <div className="absolute bottom-8 -left-7 scroll_down  animate-bounce ">
