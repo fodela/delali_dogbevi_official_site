9291519cf88baaaef77d30c3653cef6096165707
fodela <particleme30@gmail.com>
2023-03-06 12:15:21 +0000

    🛠:refactor: navigation

--- frontend/delali/components/Navigation/NavBottom/NavBottom.tsx
+++ frontend/delali/components/Navigation/NavBottom/NavBottom.tsx
@@ -3,27 +3,27 @@ import { FaLinkedinIn } from "react-icons/fa";
 
 const NavBottom = () => {
   return (
-    <div className="flex justify-between p-4">
+    <div className="flex flex-col gap-10 items-center p-4">
       <a
-        className="text-[rgb(165,165,165)] justify-center"
+        className="dark:text-white/60 text-black/50 justify-center"
         href="https://www.linkedin.com/in/delali-dogbevi/"
         target="_blank"
       >
-        <FaLinkedinIn size="20" />
+        <FaLinkedinIn size="25" />
       </a>
       <a
-        className="text-[rgb(165,165,165)] justify-center"
+        className="dark:text-white/60 text-black/50 justify-center"
         href="https://github.com/fodela"
         target="_blank"
       >
-        <BsGithub size="20" />
+        <BsGithub size="25" />
       </a>
       <a
-        className="text-[rgb(165,165,165)] justify-center"
+        className="dark:text-white/60 text-black/50 justify-center"
         href="https://twitter.com/Fodela2"
         target="_blank"
       >
-        <BsTwitter size="20" />
+        <BsTwitter size="25" />
       </a>
     </div>
   );
--- frontend/delali/components/Navigation/Navigation.tsx
+++ frontend/delali/components/Navigation/Navigation.tsx
@@ -1,26 +1,73 @@
 import Link from "next/link";
 import Image from "next/image";
-import NavigationLink from "./NavigationLink";
-import NavBottom from "./NavBottom/NavBottom";
+import { MdMenu } from "react-icons/md";
+import { useState } from "react";
+
 const Navigation = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [menuStyle, setMenuStyle] = useState("pullUp");
+
+  const handleToggleMenu = () => {
+    // isMenuOpen ? setMenuStyle("pullUp") : setMenuStyle("dropDown");
+    if (isMenuOpen) {
+      setMenuStyle("pullUp");
+      setTimeout(() => {
+        setIsMenuOpen(!isMenuOpen);
+      }, 500);
+    } else {
+      setMenuStyle("dropDown");
+      setIsMenuOpen(!isMenuOpen);
+    }
+  };
+
   return (
-    <nav className="flex flex-col h-screen w-[130px] divide-y divide-custom_white ">
-      <div className="flex flex-col">
-        {/* <Image src="logo.png" /> */}
-        <img src="logo.png" alt="" />
-
-        <p className="text-[rgb(165,165,165)] text-sm text-center py-1">
-          Delali Dogbevi
-        </p>
-      </div>
-      <NavigationLink name="About" link="/about" />
-      <NavigationLink name="Skills" link="/skills" />
-      <NavigationLink name="Work" link="/works" />
-      <NavigationLink name="Blog" link="/blog" />
-      <NavigationLink name="Contact" link="/contact" />
-
-      <NavBottom />
-    </nav>
+    <header className="flex fixed top-0  border-b border-black dark:border-white  justify-between items-center w-11/12 mx-auto pb-2 z-50 backdrop-blur-md ">
+      <Link href="/">
+        <div className="h-24 w-28 logo cursor-pointer"></div>
+      </Link>
+      <nav>
+        <ul className="hidden md:flex gap-16 items-center justify-between text-xl">
+          <li>
+            {" "}
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/portfolio">Works</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+        <button className="md:hidden">
+          <MdMenu
+            className="dark:text-white"
+            size={30}
+            onClick={handleToggleMenu}
+          />
+        </button>
+      </nav>
+      {isMenuOpen && (
+        <ul
+          className={`absolute top-full bg-zinc-100 dark:bg-zinc-800 w-full border-t  flex flex-col gap-16 px-10 py-5 justify-between text-xl ${menuStyle}`}
+        >
+          <li className="dark:hover:text-white hover:underline ">
+            <Link href="/about">About</Link>
+          </li>
+          <li className="dark:hover:text-white hover:underline">
+            <Link href="/portfolio">Works</Link>
+          </li>
+          <li className="dark:hover:text-white hover:underline">
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li className="dark:hover:text-white hover:underline">
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      )}
+    </header>
   );
 };
 
