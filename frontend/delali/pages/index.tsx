8bdb8e04e714a9add4e7b329e61d7e1a53b8f771
fodela <particleme30@gmail.com>
2023-03-17 18:25:22 +0000

    🛠:refactor: index.tsx

--- frontend/delali/pages/index.tsx
+++ frontend/delali/pages/index.tsx
@@ -7,25 +7,37 @@ import Layout from "../components/Layout/Layout";
 import Portfolio from "../components/Portfolio/Portfolio";
 import Skills from "../components/Skills/Skills";
 import Testimonial from "../components/Testimonial/Testimonial";
+// import localFont from "next/font/local";
+// const myFont = localFont({src:"./my-font.woff2"})
+
+// const customFont = myFont({
+//   subsets: ["latin"],
+//   variable: "--font-inter",
+// });
+
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>Delali Dogbevi | Home</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon/logo_icon_double_dark.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
           rel="stylesheet"
         />
       </Head>
-      <Layout>
-        <Hero />
-        <Portfolio />
-        <About />
-        <Skills />
-        <Testimonial />
-        <Contact />
-      </Layout>
+      <div
+      // className={`${fontName.variable} font-sans`}
+      >
+        <Layout>
+          <Hero />
+          <Portfolio />
+          <About />
+          <Skills />
+          <Testimonial />
+          <Contact />
+        </Layout>
+      </div>
     </>
   );
 };
